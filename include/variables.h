c0adc470329fc8c71b5745186d329c10dee42f49
Pois0nEy3 <changhunji9588@gmail.com>
2019-05-15 17:37:32 +0900

    Pornhub downloader

--- include/variables.h
+++
@@ -1,5 +0,0 @@
-global_var_currently_path=`pwd`
-global_var_videos="videos.tmp"
-global_var_temp_filename="web.tmp"
-global_var_temp="${global_var_currently_path}/tmp"
-global_var_temp_list="${global_var_currently_path}/tmp/list.tmp"
